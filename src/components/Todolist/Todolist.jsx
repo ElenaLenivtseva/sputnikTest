4c1a71f1d56f93830adba82015296e690bdbcc59
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-09-04 19:05:42 +0700

    Теперь бесконечный скролл работает полностью, с первоначальной подгрузкой. Также отрефакторены функции, немного изменена отрисовка кнопок фильтрации

--- src/components/Todolist/Todolist.jsx
+++ src/components/Todolist/Todolist.jsx
@@ -8,6 +8,12 @@ import {
 import TodoItem from "../TodoItem/TodoItem";
 import { useInView } from "react-intersection-observer";
 
+const buttons = [
+  { title: "Все", type: "all" },
+  { title: "Выполненные", type: "completed" },
+  { title: "Невыполненные", type: "uncompleted" },
+  { title: "Избранные", type: "favorite" },
+];
 const Todolist = () => {
   const { ref, inView } = useInView({
     threshold: 0.5,
@@ -17,11 +23,14 @@ const Todolist = () => {
   const todos = useSelector((state) => state.todos.todos);
   const pages = useSelector((state) => state.todos.totalPages);
   const loading = useSelector((state) => state.todos.loading);
-  const [type, setType] = useState("all");
+  const [type, setType] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   // all, favorite, completed, uncompleted
 
-  
+  useEffect(() => {
+    switchType("all");
+  }, []);
+
   useEffect(() => {
     if (inView) {
       loadMore();
@@ -57,77 +66,64 @@ const Todolist = () => {
     if (currentPage < pages) {
       const nextPage = currentPage + 1;
       setCurrentPage(nextPage);
-
-      if (type === "uncompleted") {
-        dispatch(
-          getFiltredTodosAsync({
-            page: nextPage,
-            type: "completed",
-            status: false,
-          })
-        );
-      }
-      if (type === "completed") {
-        dispatch(
-          getFiltredTodosAsync({
-            page: nextPage,
-            type: "completed",
-            status: true,
-          })
-        );
-      }
-      if (type === "favorite") {
-        dispatch(
-          getFiltredTodosAsync({
-            page: nextPage,
-            type: "favorite",
-            status: true,
-          })
-        );
-      }
-      if (type === "all") {
-        dispatch(getAllTodosLimitedAsync(nextPage));
+      console.log("nextPage", nextPage);
+      switch (type) {
+        case "uncompleted":
+          dispatch(
+            getFiltredTodosAsync({
+              page: nextPage,
+              type: "completed",
+              status: false,
+            })
+          );
+          break;
+        case "completed":
+          dispatch(
+            getFiltredTodosAsync({
+              page: nextPage,
+              type: "completed",
+              status: true,
+            })
+          );
+          break;
+        case "favorite":
+          dispatch(
+            getFiltredTodosAsync({
+              page: nextPage,
+              type: "favorite",
+              status: true,
+            })
+          );
+          break;
+        default:
+          dispatch(getAllTodosLimitedAsync(nextPage));
       }
     }
   }
 
   return (
     <div>
-      <button
-        onClick={() => {
-          switchType("all");
-        }}
-      >
-        Все
-      </button>
-      <button
-        onClick={() => {
-          switchType("completed");
-        }}
-      >
-        Выполненные
-      </button>
-      <button
-        onClick={() => {
-          switchType("uncompleted");
-        }}
-      >
-        Невыполненные
-      </button>
-      <button
-        onClick={() => {
-          switchType("favorite");
-        }}
-      >
-        Избранные
-      </button>
+      {buttons.map((item, index) => {
+        return (
+          <button
+            key={index}
+            onClick={() => {
+              switchType(item.type);
+            }}
+          >
+            {item.title}
+          </button>
+        );
+      })}
       <div>
         {todos.map((item) => {
           return <TodoItem key={item.id} item={item} />;
         })}
       </div>
       {loading && <p>Загрузка</p>}
-      {!loading && <p onClick={loadMore} ref={ref}></p>}
+      {!loading && (
+        <p onClick={loadMore} ref={ref} style={{ height: "50px" }}></p>
+      )}
     </div>
   );
 };
